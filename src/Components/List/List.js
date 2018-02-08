c50c2f6ed94bee22b990b52cefc1b0ddacbd1917
Mark <mcmcconn@gmail.com>
2018-02-08 08:59:41 -0500

    Corrected issue with bathroom uses being displayed as the current moment instead of a number of times.

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -7,11 +7,19 @@ const List = ({ data }) => {
   return (
     <ul>
       {data.map((item, index) => {
-        return (
-          <li key={index} className={styles.item}>
-            {moment(item.time).format('h:mm a')} - {item.amount || item.type || item.length || item.food}
-          </li>
-        )
+        if (typeof item.time === 'object') {
+          return (
+            <li key={index} className={styles.item}>
+              {moment(item.time).format('h:mm a')} - {item.amount || item.type || item.length || item.food}
+            </li>
+          )
+        } else {
+          return (
+            <li key={index} className={styles.item}>
+              {item.times || item.time} - {item.type || item.food}
+            </li>
+          )
+        }
       })}
     </ul>
   )
