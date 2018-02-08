c56fa867eab629ba965327acc645da3ac55f36f3
Mark <mcmcconn@gmail.com>
2018-02-07 23:17:35 -0500

    Removed excess routing code

--- src/App.js
+++ src/App.js
@@ -14,14 +14,7 @@ const App = () => {
     <Router>
       <div>
         <Route exact path='/' component={FormsNav} />
-        <Route path='/forms/:type/generalinfo' component={Form} />
-        <Route path='/forms/:type/meals' component={Form} />
-        <Route path='/forms/:type/bathroom' component={Form} />
-        <Route path='/forms/:type/naps' component={Form} />
-        <Route path='/forms/:type/supplies' component={Form} />
-        <Route path='/forms/:type/userinfo' component={Form} />
-        <Route path='/forms/:type/preview' component={Form} />
-        <Route path='/forms/:typegeneralinfo' component={Form} />
+        <Route path='/forms/:type' component={Form} />
       </div>
     </Router>
   )
